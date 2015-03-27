b006999951df6b0126762d4912e295fd8cd1e06f
Patorn Utenpattanun <patorn.u@gmail.com>
2015-03-27 13:02:49 +0700

    Fix query for rails 4 compatibility

--- lib/acts_as_many_tenants.rb
+++ lib/acts_as_many_tenants.rb
@@ -1,12 +1,12 @@
 module ActsAsManyTenants
   extend ActiveSupport::Concern
-  
+
   module ClassMethods
     def acts_as_many_tenants(association = :accounts, options = {})
       options.reverse_merge!({:through => false, :required => false, :immutable => true, :auto => true, :class_name => nil})
 
       if options[:through] && options[:required]
-        raise(ArgumentError, ":required cannot be used together with :through [ActsAsManyTenants]") 
+        raise(ArgumentError, ":required cannot be used together with :through [ActsAsManyTenants]")
       end
 
       # e.g. account_ids
@@ -34,7 +34,7 @@ module ActsAsManyTenants
       if options[:immutable]
         define_method "#{singular_ids}=" do |ids|
           if new_record?
-            super(ids) 
+            super(ids)
           else
             raise "#{association} is immutable! [ActsAsManyTenants]"
           end
@@ -57,18 +57,26 @@ module ActsAsManyTenants
       end
 
       # set the default_scope to scope to current tenant
-      # using EXISTS queries here, 
+      # using EXISTS queries here,
       # not using joins() since that would give us ReadOnly records
       unless options[:through]
         default_scope lambda {
           if ActsAsTenant.current_tenant
-            where("EXISTS (SELECT 1 FROM #{reflection.options[:join_table]} WHERE #{reflection.options[:join_table]}.#{reflection.foreign_key} = #{self.table_name}.id AND #{reflection.options[:join_table]}.#{reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            if Rails::VERSION::MAJOR >= 4
+              where("EXISTS (SELECT 1 FROM #{reflection.join_table} WHERE #{reflection.join_table}.#{reflection.foreign_key} = #{self.table_name}.id AND #{reflection.join_table}.#{reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            else
+              where("EXISTS (SELECT 1 FROM #{reflection.options[:join_table]} WHERE #{reflection.options[:join_table]}.#{reflection.foreign_key} = #{self.table_name}.id AND #{reflection.options[:join_table]}.#{reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            end
           end
         }
       else
         default_scope lambda {
           if ActsAsTenant.current_tenant
-            where("EXISTS (SELECT 1 FROM #{source_reflection.options[:join_table]} WHERE #{source_reflection.options[:join_table]}.#{source_reflection.foreign_key} = #{self.table_name}.#{through_reflection.foreign_key} AND #{source_reflection.options[:join_table]}.#{source_reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            if Rails::VERSION::MAJOR >= 4
+              where("EXISTS (SELECT 1 FROM #{through_reflection.table_name} WHERE #{through_reflection.table_name}.#{through_reflection.foreign_key} = #{self.table_name}.id AND #{through_reflection.table_name}.#{source_reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            else
+              where("EXISTS (SELECT 1 FROM #{source_reflection.options[:join_table]} WHERE #{source_reflection.options[:join_table]}.#{source_reflection.foreign_key} = #{self.table_name}.#{through_reflection.foreign_key} AND #{source_reflection.options[:join_table]}.#{source_reflection.association_foreign_key} = ?)", ActsAsTenant.current_tenant.id)
+            end
           end
         }
       end
